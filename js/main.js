95f287f68a7805bc1b675cee46c8148fcfac21a7
CubeSky <max_301@live.com>
2018-04-12 10:57:04 +0800

    Kefjs 0.7.1-K3

--- js/main.js
+++ js/main.js
@@ -97,7 +97,7 @@ const dataStore = [{
     source: 'Github',
     type: 'jar',
     license: 'MIT',
-    version: ['0.7.1', '0.7.1-K2']
+    version: ['0.7.1', '0.7.1-K2', '0.7.1-K3']
 }, {
     project: 'KPagejs',
     group: 'party.liyin',
